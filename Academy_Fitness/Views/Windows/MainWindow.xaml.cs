173172ae0fbc83c187d7d48cd0bfdfc0516d6eae
Murad_777 <murad.gadzhiev1999@mail.ru>
2022-01-02 15:44:45 +0300

    v1.0

---
+++ Academy_Fitness/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Academy_Fitness.Model;
+
+namespace Academy_Fitness
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Academy_fitnessEntities context;
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new Academy_fitnessEntities();
+            DataGridRegistration.ItemsSource = context.CourseRegistration.ToList();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+ 
+            var newRegistration = new CourseRegistration();
+            context.CourseRegistration.Add(newRegistration);
+            var win = new WindowCourseRegistration(context, newRegistration);
+            win.ShowDialog();
+            DataGridRegistration.ItemsSource = context.CourseRegistration.ToList();
+        }
+
+        private void BtnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            var remove = DataGridRegistration.SelectedItem as CourseRegistration;
+            if(remove == null)
+            {
+                MessageBox.Show("Строка не выбрана!");
+                return;
+            }
+            else
+            {
+                context.CourseRegistration.Remove(remove);
+                context.SaveChanges();
+                DataGridRegistration.ItemsSource = context.CourseRegistration.ToList();
+                MessageBox.Show("Данные успешно удалены!", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Button btnEdit = sender as Button;
+            var currentRegistration = btnEdit.DataContext as CourseRegistration;
+            var win = new WindowCourseRegistration(context,currentRegistration);
+            win.ShowDialog();
+        }
+    }
+}
---
+++ Academy_Fitness/WindowCourseRegistration.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Academy_Fitness.Model;
+
+namespace Academy_Fitness
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowCourseRegistration.xaml
+    /// </summary>
+    public partial class WindowCourseRegistration : Window
+    {
+
+        Academy_fitnessEntities context;
+        public WindowCourseRegistration(Academy_fitnessEntities context, CourseRegistration currentRegistartion)
+        {
+            InitializeComponent();
+            this.context = context;
+            CmbCourse.ItemsSource = context.Course.ToList();
+            CmbTrainer.ItemsSource = context.Trainer.ToList();
+
+            this.DataContext = currentRegistartion;
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if(CheckRegistration())
+            {
+                context.SaveChanges();
+                this.Close();
+                MessageBox.Show("Данные успешно добавлены!", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+          
+        }
+
+        private bool CheckRegistration()
+        {
+            var reg = this.DataContext as CourseRegistration;
+            if (reg.Trainer == null)
+            {
+                MessageBox.Show("Тренер не выбран!");
+                return false;
+            }
+            if (reg.Course == null)
+            {
+                MessageBox.Show("Курсы не выбраны!");
+                return false;
+            }
+            if (reg.CreatedDate == null)
+            {
+                MessageBox.Show("Дата не выбрана!");
+                return false;
+            }
+
+            if (!int.TryParse(TxtTotalPoint.Text, out int result))
+            {
+                MessageBox.Show("Неверные данные!");
+                return false;
+            }
+            return true;
+        }
+
+
+
+
+
+    }
+}
